ce05d88fbb9724205b1ecca104e9939051f53270
sajal-1999 <sajal52@gmail.com>
2022-10-04 16:33:00 +0530

    Initial Commit

---
+++ src/main/java/com/newordle/newordle/NewordleApplication.java
@@ -0,0 +1,14 @@
+package com.newordle.newordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewordleApplication.class, args);
+		System.out.println("Initial Commit");
+	}
+
+}
