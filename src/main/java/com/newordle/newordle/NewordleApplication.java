065ac83eb0be6f2dc50ab9430c402394c227fde9
sajal <sajal52@gmail.com>
2022-10-16 20:10:26 +0530

    Updating startup log message

--- src/main/java/com/newordle/newordle/NewordleApplication.java
+++ src/main/java/com/newordle/newordle/NewordleApplication.java
@@ -8,7 +8,7 @@ public class NewordleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewordleApplication.class, args);
-		System.out.println("Let the games begin!");
+		System.out.println("Let the game begin!");
 	}
 
 }
