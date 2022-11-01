e7bbb4a614ede4053ce243ec97add2fdb26583ae
sajal <sajal52@gmail.com>
2022-11-01 20:26:13 +0530

    Cleanup for mongo migration

--- src/main/java/com/newordle/newordle/services/NewordleService.java
+++ src/main/java/com/newordle/newordle/services/NewordleService.java
@@ -6,16 +6,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import org.bson.Document;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
-import com.mongodb.ConnectionString;
 import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.InsertOneResult;
+import com.newordle.newordle.dao.WordsDao;
 
 @Service
 public class NewordleService {
@@ -23,6 +19,7 @@ public class NewordleService {
     static Map<Character, List<Integer>> dailyMap = new HashMap<>();
     static Set<String> wordSet = new HashSet<>();
     static Set<Integer> wordsUsedIndices = new HashSet<>();
+    WordsDao wordsDao = new WordsDao();
 
     // NewordleService() empty constructor to create a char map for the daily word
     public NewordleService() {
@@ -92,8 +89,8 @@ public class NewordleService {
         return false;
     }
 
-    // 0-grey, 1-yellow, 2-green
-    public int[] getResult(String entered) {
+    // getResult checks the entered word against the daily word
+    public int[] getResult(String enteredWord) {
         Map<Character, List<Integer>> dailyMapCopy = new HashMap<>();
         dailyMapCopy = dailyMap
                 .entrySet()
@@ -101,9 +98,10 @@ public class NewordleService {
                 .collect(Collectors.toMap(Map.Entry::getKey, valueMapper -> new ArrayList<>(valueMapper.getValue())));
 
         int[] res = new int[5];
+        // 0-grey, 1-yellow, 2-green
         for (int i = 0; i < 5; i++) {
             char c = dailyWord.charAt(i);
-            if (c == entered.charAt(i)) {
+            if (c == enteredWord.charAt(i)) {
                 res[i] = 2;
                 dailyMapCopy.get(c).remove(Integer.valueOf(i));
             }
@@ -115,7 +113,7 @@ public class NewordleService {
         // now computing yellow and grey
         for (int i = 0; i < 5; i++) {
             if (res[i] == 0) {
-                char c = entered.charAt(i);
+                char c = enteredWord.charAt(i);
                 if (dailyMapCopy.containsKey(c) && !dailyMapCopy.get(c).isEmpty()) {
                     res[i] = 1;
                     dailyMapCopy.get(c).remove(0);
@@ -127,36 +125,39 @@ public class NewordleService {
         return res;
     }
 
+    // updateMongoCollection is used to add the words for first time to mongoDB
+    // collection
     public void updateMongoCollection() {
         try {
             ClassLoader classLoader = this.getClass().getClassLoader();
             File wordFile = new File(classLoader.getResource("static/Words.txt").getFile());
             Scanner wordFileScanner = new Scanner(wordFile);
-            ConnectionString connectionString = new ConnectionString(
-                    "mongodb+srv://newordleadmin:ZygRC0kwQ17PiZRp@newordle.a043wfu.mongodb.net/?retryWrites=true&w=majority");
-            try {
-                MongoClient mongoClient = MongoClients.create(connectionString);
-                MongoDatabase database = mongoClient.getDatabase("newordle");
-                MongoCollection<Document> collection = database.getCollection("words");
-                String word;
-                int i = 0;
-                while (wordFileScanner.hasNextLine()) {
-                    i++;
-                    word = wordFileScanner.nextLine().strip().toLowerCase();
-                    try {
-                        InsertOneResult result = collection.insertOne(new Document()
-                                .append("_id", new ObjectId())
-                                .append("index", i)
-                                .append("word", word));
-                        System.out.println("Success! Inserted document id: " + result.getInsertedId());
-                    } catch (MongoException me) {
-                        System.err.println("Unable to insert due to an error: " + me);
-                    }
-                }
+
+            MongoCollection<Document> collection = wordsDao.getDbCollection();
+            if (collection == null) {
+                System.out.println("Failed to insert!");
                 wordFileScanner.close();
-            } catch (MongoException mException) {
-                System.out.println(mException);
+                return;
             }
+            String word;
+            int i = 0;
+            Document doc;
+            while (wordFileScanner.hasNextLine()) {
+                i++;
+                word = wordFileScanner.nextLine().strip().toLowerCase();
+                try {
+                    doc = new Document().append("_id", i)
+                            .append("used", false)
+                            .append("word", word);
+                    InsertOneResult result = collection.insertOne(doc);
+                    System.out.println("Success! Inserted document id: " +
+                            result.getInsertedId());
+
+                } catch (MongoException me) {
+                    System.err.println("Unable to insert due to an error: " + me);
+                }
+            }
+            wordFileScanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
