60e8d9585923542e5361013bee22567186ad4745
sajal <sajal52@gmail.com>
2022-11-02 10:16:59 +0530

    Adding functions for migration to mongo

--- src/main/java/com/newordle/newordle/dao/WordsDao.java
+++ src/main/java/com/newordle/newordle/dao/WordsDao.java
@@ -1,15 +1,23 @@
 package com.newordle.newordle.dao;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bson.Document;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientException;
 import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.newordle.newordle.model.WordsDb;
+import org.bson.conversions.Bson;
+import static com.mongodb.client.model.Updates.*;
 
 public class WordsDao {
 
@@ -18,8 +26,7 @@ public class WordsDao {
     // establishConnection sets the Mongo Client connection
     private void establishConnection() {
         ConnectionString connectionString = new ConnectionString(
-                "mongodb+srv://newordleadmin:ZygRC0kw17PiZRp@newordle.a043wfu.mongodb.net/?retryWrites=true&w=majority");
-
+                "mongodb+srv://newordleadmin:WFiVDY6zw0Wtpcru@newordle.a043wfu.mongodb.net/?retryWrites=true&w=majority");
         mongoClient = MongoClients.create(connectionString);
     }
 
@@ -29,7 +36,7 @@ public class WordsDao {
         try {
             establishConnection();
             MongoDatabase database = mongoClient.getDatabase("newordle");
-            MongoCollection<Document> collection = database.getCollection("wordss");
+            MongoCollection<Document> collection = database.getCollection("words");
             return collection;
         } catch (IllegalArgumentException iae) {
             System.out.println("Error Establishing Connection!");
@@ -46,8 +53,64 @@ public class WordsDao {
         }
     }
 
+    // terminateConnection sets the Mongo Client connection
+    public void terminateConnection() {
+        mongoClient.close();
+    }
+
     // findWord returns the document corresponding to the _id passed as POJO. TBI
     public WordsDb findWordById(int _id) {
-        return new WordsDb(1, "", true);
+        try {
+            MongoCollection<Document> collection = getDbCollection();
+            Document foundWordDoc = collection.find(Filters.eq("_id", _id)).first();
+            if (foundWordDoc == null) {
+                return null;
+            }
+            return new WordsDb(_id, (String) foundWordDoc.get("word"), (Boolean) foundWordDoc.get("used"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            terminateConnection();
+        }
+    }
+
+    // getAllWords returns set of all words from the DB
+    public Set getAllWords() {
+        try {
+            MongoCollection<Document> collection = getDbCollection();
+            FindIterable<Document> iterDocs = collection.find();
+            MongoCursor<Document> cursor = iterDocs.iterator();
+            int i = 0;
+            Set<String> wordSet = new HashSet<>();
+            while (cursor.hasNext()) {
+                i++;
+                wordSet.add((String) cursor.next().get("word"));
+            }
+            return wordSet;
+        } catch (Exception e) {
+            System.out.println("Error fetching words!");
+            return null;
+        } finally {
+            terminateConnection();
+        }
+    }
+
+    // setUsed sets the daily word's used status to true
+    public void setUsed(int _id) {
+        try {
+            MongoCollection<Document> collection = getDbCollection();
+            Bson update = set("used", true);
+            Document foundWordDoc = collection.findOneAndUpdate(Filters.eq("_id", _id), update);
+            if (foundWordDoc == null) {
+                System.err.println("Failed to update status!");
+                return;
+            }
+            System.out.println(findWordById(_id));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            terminateConnection();
+        }
     }
 }
