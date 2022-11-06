cde1a77176db04e5bd426cd5cbcc0d4fe5d83386
sajal <sajal52@gmail.com>
2022-11-06 19:28:31 +0530

    Cleanup and optimizations

--- src/main/java/com/newordle/newordle/controllers/Controller.java
+++ src/main/java/com/newordle/newordle/controllers/Controller.java
@@ -7,6 +7,7 @@ import com.newordle.newordle.services.NewordleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,13 +30,18 @@ public class Controller {
     }
 
     // sample endpoint for first load of words to mongodb. to be deprecated.
-    @GetMapping("/updateMongoCollection")
-    public String updateMongoCollection() {
-        service.updateMongoCollection();
-        return "Ran success";
-    }
+    // @PutMapping("/insertAllWords")
+    // public String insertAllWords() {
+    // service.insertAllWords();
+    // return "Ran success";
+    // }
 
     // curl-X GET localhost:8080/newordle?queryParam=madam
+    /*
+     * --------TO DO--------
+     * Find better name
+     * Response Codes update
+     */
     @GetMapping("/newordle")
     public String newordle(@RequestParam String queryParam) {
         if (!service.validateEnteredWord(queryParam)) {
@@ -53,6 +59,17 @@ public class Controller {
             }
         }
         return ret;
+    }
+
+    // API to insert one new word
+    /*
+     * --------TO DO--------
+     * Find better name
+     * Response Codes update
+     */
+    @PutMapping("/insertWord")
+    public String insertWord(@RequestParam String word) {
+        return service.insertOneWord(word);
     }
 
     // sample api to check frontend
