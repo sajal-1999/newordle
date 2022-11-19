d81f6e7abdeb34cb35ab4e199205acf535f1df10
sajal <sajal52@gmail.com>
2022-11-19 17:42:29 +0530

    Adding backspace logic

--- newordle-frontend/src/components/WordGrid.js
+++ newordle-frontend/src/components/WordGrid.js
@@ -3,11 +3,11 @@ import WordRow from "./WordRow";
 
 function WordGrid() {
     const [curRow, setCurRow] = useState(1);
-    
+
     useEffect(() => {
         const detectKeyUp = (e) => {
-            if(e.key==="Enter") {
-                setCurRow(row => row+1)
+            if (e.key === "Enter") {
+                setCurRow(row => row + 1)
             }
             //TODO: add callback and revert to same row if not correct
         }
@@ -16,13 +16,12 @@ function WordGrid() {
     }, []);
 
     return <div>
-        <WordRow active={curRow.valueOf() >= 1} id={1} />
-        <WordRow active={curRow.valueOf() >= 2} id={2} />
-        <WordRow active={curRow.valueOf() >= 3} id={3} />
-        <WordRow active={curRow.valueOf() >= 4} id={4} />
-        <WordRow active={curRow.valueOf() >= 5} id={5} />
-        <WordRow active={curRow.valueOf() >= 6} id={4} />
-        <WordRow active={curRow.valueOf() >= 7} id={5} />
+        <WordRow active={curRow.valueOf() === 1} displayed={curRow.valueOf() >= 1} />
+        <WordRow active={curRow.valueOf() === 2} displayed={curRow.valueOf() >= 2} />
+        <WordRow active={curRow.valueOf() === 3} displayed={curRow.valueOf() >= 3} />
+        <WordRow active={curRow.valueOf() === 4} displayed={curRow.valueOf() >= 4} />
+        <WordRow active={curRow.valueOf() === 5} displayed={curRow.valueOf() >= 5} />
+        <WordRow active={curRow.valueOf() === 6} displayed={curRow.valueOf() >= 6} />
     </div>
 }
 
--- newordle-frontend/src/components/WordRow.js
+++ newordle-frontend/src/components/WordRow.js
@@ -1,44 +1,8 @@
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 import Cell from "./Cell";
 
-function usePrevious(value) {
-    const ref = useRef();
-  
-    useEffect(() => {
-      ref.current = value;
-    }, [value]);
-    
-    return ref.current;
-  }
-  
-
-function useEffectAllDepsChange(fn, deps) {
-    const prevDeps = usePrevious(deps);
-    const changeTarget = useRef();
-
-    useEffect(() => {
-        // nothing to compare to yet
-        if (changeTarget.current === undefined) {
-            changeTarget.current = prevDeps;
-        }
-
-        // we're mounting, so call the callback
-        if (changeTarget.current === undefined) {
-            return fn();
-        }
-
-        // make sure every dependency has changed
-        if (changeTarget.current.every((dep, i) => dep !== deps[i])) {
-            changeTarget.current = deps;
-
-            return fn();
-        }
-    }, [fn, prevDeps, deps]);
-}
-
 function WordRow(props) {
-    // console.log(props.id + " - " + props.active);
     const [word, setWord] = useState("");
     const [cell, setCell] = useState(0);
     const [key, setKey] = useState("");
@@ -48,54 +12,60 @@ function WordRow(props) {
     const [letter4, setLetter4] = useState("");
     const [letter5, setLetter5] = useState("");
 
-    useEffectAllDepsChange(() => {
-        switch (cell) {
-            case 1: setLetter1(key)
+    useEffect(() => {
+        var curCell = cell;
+        if (`${key}` === "")
+            curCell += 1;
+        switch (curCell) {
+            case 1: setLetter1(`${key}`)
                 break;
-            case 2: setLetter2(key)
+            case 2: setLetter2(`${key}`)
                 break;
-            case 3: setLetter3(key)
+            case 3: setLetter3(`${key}`)
                 break;
-            case 4: setLetter4(key)
+            case 4: setLetter4(`${key}`)
                 break;
-            case 5: setLetter5(key)
+            case 5: setLetter5(`${key}`)
                 break;
             default:
                 break;
         }
-    }, [cell, key]);
+        setKey("0");
+    }, [cell]);
 
     useEffect(() => {
-        if (key !== "") {
-            setCell(prevCell => prevCell + 1);
-            setWord(prevWord => prevWord + key);
-        }
-    }, [key]);
-
-    useEffect(() => {
-        const detectKeyDown = (e) => {
-            if (props.active && new RegExp('^[a-zA-Z]$').test(e.key)) {
-                setKey(e.key);
+        const detectKeyUp = (e) => {
+            if (props.active) {
+                if (word.length < 5 && new RegExp('^[a-zA-Z]$').test(e.key)) {
+                    setKey(e.key); //a-z
+                    setCell(prevCell => prevCell + 1);
+                    setWord(prevWord => prevWord + e.key);
+                } else if (e.key === 'Backspace' && word.length > 0) {
+                    setKey("");
+                    setCell(prevCell => prevCell - 1);
+                    setWord(prevWord => prevWord.slice(0, -1));
+                } else if (e.key === 'Enter' && word.length === 5) {
+                    console.log(word); //check word
+                } else {
+                    console.log("invalid input");
+                }
             }
         }
-        document.addEventListener('keydown', detectKeyDown, true)
-    }, [props.active]);
+        document.addEventListener('keyup', detectKeyUp, false)
+        return () => document.removeEventListener("keyup", detectKeyUp);
+    }, [props.active, word]);
 
 
     return (
-        <Row style={{ height: '3rem', justifyContent: 'center' }} >
-            <Cell active={props.active && cell.valueOf() >= 1} text={letter1} rowid={props.id} cell={1} />
-            <Cell active={props.active && cell.valueOf() >= 2} text={letter2} rowid={props.id} cell={2} />
-            <Cell active={props.active && cell.valueOf() >= 3} text={letter3} rowid={props.id} cell={3} />
-            <Cell active={props.active && cell.valueOf() >= 4} text={letter4} rowid={props.id} cell={4} />
-            <Cell active={props.active && cell.valueOf() >= 5} text={letter5} rowid={props.id} cell={5} />
-            <p>{word}</p>
-            {/* {Array.from({ length: 5 }).map((_, idx) =>
-                <Cell key={idx} active={false} letter={letter1} setLetter={setLetter} />
-            )} */}
+        <Row style={{ height: '5rem', justifyContent: 'center' }} >
+            <Cell active={props.displayed && cell.valueOf() >= 1} text={letter1} />
+            <Cell active={props.displayed && cell.valueOf() >= 2} text={letter2} />
+            <Cell active={props.displayed && cell.valueOf() >= 3} text={letter3} />
+            <Cell active={props.displayed && cell.valueOf() >= 4} text={letter4} />
+            <Cell active={props.displayed && cell.valueOf() >= 5} text={letter5} />
+            <p style={{ color: 'red' }}>{word}</p>
         </Row>
     )
 }
-
 
 export default WordRow;
