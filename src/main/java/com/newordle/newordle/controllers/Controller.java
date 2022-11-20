e8cc5f74f3b137498ef8ca491fa12ebcfeb700f5
sajal <sajal52@gmail.com>
2022-11-20 22:08:05 +0530

    Fixing CORS issue

--- src/main/java/com/newordle/newordle/controllers/Controller.java
+++ src/main/java/com/newordle/newordle/controllers/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.newordle.newordle.services.NewordleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class Controller {
 
     @Autowired
@@ -44,6 +46,7 @@ public class Controller {
      */
     @GetMapping("/newordle")
     public String newordle(@RequestParam String queryParam) {
+        queryParam = queryParam.strip().toLowerCase();
         if (!service.validateEnteredWord(queryParam)) {
             return "Invalid Word!";
         }
